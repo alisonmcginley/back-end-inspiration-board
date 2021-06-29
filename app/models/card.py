5ecc4fe28707831769d6d6ad0b9f102f00d591f7
alison <alimcginley@gmail.com>
2021-06-29 13:22:16 -0700

    adds board_id to card_to_json

--- app/models/card.py
+++ app/models/card.py
@@ -10,5 +10,6 @@ class Card(db.Model):
         return {
             "card_id": self.card_id,
             "message": self.message,
-            "likes_count": self.likes_count
+            "likes_count": self.likes_count,
+            "board_id": self.board_id
         }
